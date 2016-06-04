d413dcaeab9a8ccff722b9fe3af01b6d3e85d64e
pudda 1 <chalithamaduranga>
2016-06-04 04:31:21 -0700

    advance report db

---
+++ app/AdvanceProgram.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AdvanceProgram extends Model
+{
+    //
+    protected $fillable = [
+        'emp_id', 'handOverDate', 'approvedDate','Status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+}
